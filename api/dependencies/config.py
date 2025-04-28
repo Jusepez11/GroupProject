669b57fc252614c2c25f3aab02f281d9f3c5abb5
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-04-28 18:11:52 -0400

    gitngore config.py

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
     db_host = "localhost"
-    db_name = "sandwich_maker_api"
+    db_name = "sandwich_maker"
     db_port = 3306
-    db_user = "root"
-    db_password = "W00dy#122004"
+    db_user = "user"
+    db_password = "password"
     app_host = "localhost"
     app_port = 8000
