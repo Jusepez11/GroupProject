dbfdd230158919c7ef29bbfed382f5b3254bc5b0
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-04-28 14:09:21 -0400

    Create promotions.py

---
+++ api/routers/promotions.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends, FastAPI, status, Response
+from sqlalchemy.orm import Session
+from ..controllers import promotion as controller
+from ..schemas import promotion as schema
+from ..dependencies.database import engine, get_db
+
+router = APIRouter(
+    tags=['Promotions'],
+    prefix="/promotions"
+)
+
+
+@router.get("/", response_model=list[schema.Promotion])
+def read_all(db: Session = Depends(get_db)):
+    return controller.read_all(db)
+
+@router.get("/{item_id}", response_model=schema.Promotion)
+def read_one(item_id: int, db: Session = Depends(get_db)):
+    return controller.read_one(db, item_id=item_id)
+
+@router.post("/", response_model=schema.Promotion)
+def create(request: schema.PromotionCreate, db: Session = Depends(get_db)):
+    return controller.create(db=db, request=request)
+
+@router.delete("/{item_id}")
+def delete(item_id: int, db: Session = Depends(get_db)):
+    return controller.delete(db=db, item_id=item_id)
+
+
+@router.put("/{item_id}", response_model=schema.Promotion)
+def update(item_id: int, request: schema.Promotion, db: Session = Depends(get_db)):
+    return controller.update(db=db, request=request, item_id=item_id)
