add1df8eba7bc1cec982a4f7be1ead229fb485a0
Sebastian Lopez-Gallardo <44486060+Jusepez11@users.noreply.github.com>
2025-05-03 16:42:28 -0400

    created controllers and routers, but need tweaking

---
+++ api/controllers/menu_items.py
@@ -0,0 +1,71 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import menu_items as model
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def create(db: Session, request):
+    new_item = model.MenuItems(
+        item_ID=request.item_ID,
+        item_name=request.item_name,
+        item_description=request.item_description,
+        item_price=request.item_price,
+        item_category=request.item_category,
+        item_ingredients=request.item_ingredients,
+    )
+    try:
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    return new_item
+
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.MenuItems).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
+
+
+def read_one(db: Session, item_id):
+    try:
+        item = db.query(model.MenuItems).filter(model.MenuItems.item_ID == item_id).first()
+        if not item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item
+
+
+def update(db: Session, item_id, request):
+    try:
+        item = db.query(model.MenuItems).filter(model.MenuItems.item_ID == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item.first()
+
+
+def delete(db: Session, item_id):
+    try:
+        item = db.query(model.MenuItems).filter(model.MenuItems.item_ID == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
