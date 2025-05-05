b4626af3459d10511a8f1190f6e6083f73a60ed4
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-05-04 20:06:02 -0400

    Update service_rep.py

--- api/schemas/service_rep.py
+++ api/schemas/service_rep.py
@@ -1,19 +1,23 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
-from .sandwiches import Sandwich
+#from .sandwiches import S
 
 # Base schema with shared attributes
 class ServiceRepresentativeBase(BaseModel):
-    employeeID: int
     name: str
 
 # Schema for creating a new service rep
 class ServiceRepresentativeCreate(ServiceRepresentativeBase):
     pass
 
+
+# Schema for creating a new service rep
+class ServiceRepresentativeUpdate(BaseModel):
+    name: Optional[str] = None
+
 # Schema for reading data, with config for ORM mode
-class ServiceRepresentative(ServiceRepresentativeBase):
+class ServiceRepresentativeRead(ServiceRepresentativeBase):
     employeeID: int
 
     class ConfigDict:
