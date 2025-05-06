f460b1c5ac2592d4ce5561c536a6aabf65d11bd0
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-05-05 22:07:54 -0400

    removed sandwich

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources, promotion, restaurant_manager, customer, payment, service_rep, menu_items
+from . import orders, order_details, recipes, resources, promotion, restaurant_manager, customer, payment, service_rep, menu_items
 #from .menu_items import menu_items
 
 from ..dependencies.database import engine
@@ -8,7 +8,6 @@ def index():
     orders.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
     recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
     promotion.Base.metadata.create_all(engine)
     restaurant_manager.Base.metadata.create_all(engine)
